63a5ea8d836dcc77ea4d3f1fa50dad5b8c1edef3
chamindu0 <igcysenarathna@gmail.com>
2024-02-29 21:27:37 +0530

    update responsivnes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -39,7 +39,7 @@ const Portfolio = () => {
         },
     ]
   return (
-    <div name="Portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+    <div name="Portfolio" className=' h-full bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pt-96 h-full lg:pt-0 md:pt-0' >
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
